c0200ffbf65f817f568526385afac081d65fdd78
zignich <zignich.ds@gmail.com>
2019-07-22 17:42:25 +0600

    added works to portfolio

--- src/pages/works/graphicdesign.js
+++ src/pages/works/graphicdesign.js
@@ -2,13 +2,21 @@ import { graphql } from 'gatsby'
 import React from 'react'
 import get from 'lodash/get'
 import { Link } from 'gatsby'
+import Img from 'gatsby-image'
 
 import Meta from 'components/Meta'
 import Layout from 'components/Layout'
 
-import image1 from '../../assets/images/cover/img_contacts.png'
-
 const GraphicDesign = ({ data, location }) => {
+
+  const image1 = get(data, 'image1.childImageSharp.sizes')
+  const image2 = get(data, 'image2.childImageSharp.sizes')
+  const image3 = get(data, 'image3.childImageSharp.sizes')
+  const image4 = get(data, 'image4.childImageSharp.sizes')
+  const image5 = get(data, 'image5.childImageSharp.sizes')
+  const image6 = get(data, 'image6.childImageSharp.sizes')
+  const image7 = get(data, 'image7.childImageSharp.sizes')
+  const image8 = get(data, 'image8.childImageSharp.sizes')
 
   return (
     <Layout location={location}>
@@ -19,7 +27,32 @@ const GraphicDesign = ({ data, location }) => {
             <h1 className="font-weight-light text-center">Graphic design</h1>
             <hr />
             <div className="row">
-              <img src={image1} className="col-md-12 col-sm-12"/>
+
+              <div className="card col-md-12 col-sm-12 pt-3 mt-2">
+                <Img sizes={image1} className="mb-3" />
+              </div>
+              <div className="card col-md-12 col-sm-12 pt-3 mt-2">
+                <Img sizes={image2} className="mb-3" />
+              </div>
+              <div className="card col-md-12 col-sm-12 pt-3 mt-2">
+                <Img sizes={image3} className="mb-3" />
+              </div>
+              <div className="card col-md-12 col-sm-12 pt-3 mt-2">
+                <Img sizes={image4} className="mb-3" />
+              </div>
+              <div className="card col-md-12 col-sm-12 pt-3 mt-2">
+                <Img sizes={image5} className="mb-3" />
+              </div>
+              <div className="card col-md-12 col-sm-12 pt-3 mt-2">
+                <Img sizes={image6} className="mb-3" />
+              </div>
+              <div className="card col-md-12 col-sm-12 pt-3 mt-2">
+                <Img sizes={image7} className="mb-3" />
+              </div>
+              <div className="card col-md-12 col-sm-12 pt-3 mt-2">
+                <Img sizes={image8} className="mb-3" />
+              </div>
+
             </div>
           </div>
         </section>
@@ -40,5 +73,62 @@ export const pageQuery = graphql`
         author
       }
     }
+    image1: file(relativePath: { eq: "graphicDesign/gr_design_1.jpg" }) {
+      childImageSharp {
+        sizes(quality: 100) {
+          ...GatsbyImageSharpSizes_withWebp
+        }
+      }
+    }
+    image2: file(relativePath: { eq: "graphicDesign/gr_design_2.jpg" }) {
+      childImageSharp {
+        sizes(quality: 100) {
+          ...GatsbyImageSharpSizes_withWebp
+        }
+      }
+    }
+    image3: file(relativePath: { eq: "graphicDesign/gr_design_3.jpg" }) {
+      childImageSharp {
+        sizes(quality: 100) {
+          ...GatsbyImageSharpSizes_withWebp
+        }
+      }
+    }
+    image4: file(relativePath: { eq: "graphicDesign/gr_design_4.jpg" }) {
+      childImageSharp {
+        sizes(quality: 100) {
+          ...GatsbyImageSharpSizes_withWebp
+        }
+      }
+    }
+    image5: file(relativePath: { eq: "graphicDesign/gr_design_5.jpg" }) {
+      childImageSharp {
+        sizes(quality: 100) {
+          ...GatsbyImageSharpSizes_withWebp
+        }
+      }
+    }
+    image6: file(relativePath: { eq: "graphicDesign/gr_design_6.jpg" }) {
+      childImageSharp {
+        sizes(quality: 100) {
+          ...GatsbyImageSharpSizes_withWebp
+        }
+      }
+    }
+    image7: file(relativePath: { eq: "graphicDesign/gr_design_7.jpg" }) {
+      childImageSharp {
+        sizes(quality: 100) {
+          ...GatsbyImageSharpSizes_withWebp
+        }
+      }
+    }
+    image8: file(relativePath: { eq: "graphicDesign/gr_design_8.jpg" }) {
+      childImageSharp {
+        sizes(quality: 100) {
+          ...GatsbyImageSharpSizes_withWebp
+        }
+      }
+    }
+
   }
 `
